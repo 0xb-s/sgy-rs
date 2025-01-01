7dd1ab341b0cfb33b41e4ba46b1bc7e09f257418
0xb-s <145866191+0xb-s@users.noreply.github.com>
2024-12-31 19:31:13 -0800

    Create value.rs

---
+++ src/value.rs
@@ -0,0 +1,11 @@
+/// The size (in bytes) of each trace header in a SEG-Y file.
+
+pub const TRACE_HEADER_SIZE: usize = 240;
+
+/// The size  of the textual EBCDIC header in a SEG-Y file.
+
+pub const EBCDIC_HEADER_SIZE: usize = 3200;
+
+/// The size (in bytes) of the SEG-Y binary header.
+
+pub const BINARY_HEADER_SIZE: usize = 400;
